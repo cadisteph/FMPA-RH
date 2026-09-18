53a30b3f2d68cf645e2a1db40461a5b9e81af120
cadisteph <cadisteph@hotmail.fr>
2026-09-18 13:45:12 +0200

    Update script_saisie.js

--- script_saisie.js
+++ script_saisie.js
@@ -2234,7 +2234,7 @@ function appliquerMasquageFeuilles(workbook) {
     if (!workbook || !workbook.SheetNames) return;
 
     // Renseigne ici le nom EXACT de tes onglets sensibles (attention aux majuscules/espaces)
-    const feuillesACacher = ["Agents", "Parametres", "Donnees_Brutes"];
+    const feuillesACacher = ["baseAgents", "historiqueSuivi", "Parametres"];
 
     feuillesACacher.forEach(nomFeuille => {
         const sheetIndex = workbook.SheetNames.indexOf(nomFeuille);
@@ -2244,7 +2244,7 @@ function appliquerMasquageFeuilles(workbook) {
             if (!workbook.Workbook.Sheets[sheetIndex]) workbook.Workbook.Sheets[sheetIndex] = {};
             
             // Si afficherOngletsSecurises est vrai => 0 (Visible), sinon => 2 (VeryHidden)
-            workbook.Workbook.Sheets[sheetIndex].Hidden = afficherOngletsSecurises ? 0 : 2;
+            workbook.Workbook.Sheets[sheetIndex].Hidden = afficherOngletsSecurises ? 0 : 0;
         }
     });
 }
